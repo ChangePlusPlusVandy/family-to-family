4730255ee6509aa7aa295e980a97c567b154cefa
“OliviaLiu” <xiaohan.liu.1@vanderbilt.edu>
2022-11-16 23:36:39 -0600

    home layout nav bar

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -7,3 +7,17 @@ export const STATUS_CODE = {
     SUCESS : "SUCESS",
     ERROR: "ERROR"
 }
+
+export const ROUTES = {
+    HOME: "/home",
+    PROFILE : "/home/profile",
+    RESOURCES : "/home/resources",
+    FAQ: "home/faq"
+}
+
+export const HOME_NAV_LABELS = {
+    HOME: "Home",
+    PROFILE : "Profile",
+    RESOURCES : "Resources",
+    FAQ: "FAQ"
+}
