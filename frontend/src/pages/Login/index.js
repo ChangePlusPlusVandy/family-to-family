0b2e3ec24a358c6ff6520e09ed873747df7dd6db
Gabriel Dong <gabrielhdong@gmail.com>
2022-12-21 20:56:53 -0600

    barebone login page

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -62,3 +62,28 @@
 // };
 
 // export default Login;
+
+import React from "react";
+import logo from "./F2F-logo.png";
+import "./index.css";
+
+export default function Login() {
+  return (
+    <div>
+      <img src={logo} />
+      <h1 className="title">Login</h1>
+      <p className="description">Please sign in to continue</p>
+      <form>
+        <p>Email</p>
+        <input type="text" placeholder="" />
+        <p>Password</p>
+        <input type="text" placeholder="" />
+        <input type="checkbox" />
+        <p>Forgot Password?</p>
+        <div className="submitButton">
+          <p>Log In</p>
+        </div>
+      </form>
+    </div>
+  );
+}
