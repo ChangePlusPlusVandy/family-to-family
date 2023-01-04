f6e8a4efd020f04ecbb8c75b9dd88cc914c4d620
Allan Zhang <allanwzhang04@gmail.com>
2023-01-03 19:43:31 -0500

    working home page (hard coded with some unresolved future problems

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect, createContext } from "react";
 import { Link } from "react-router-dom";
-import classNames from 'classnames/bind';
+import classNames from "classnames/bind";
 import { useAuth } from "../../lib/AuthContext";
 import styles from "./index.module.css";
 import { NavBar } from "../NavBar";
+import { ROUTES } from "../../lib/constants";
+
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
+
 const cx = classNames.bind(styles);
 
 export const Home = () => {
@@ -40,20 +45,57 @@ export const Home = () => {
   const lastName = "Adams";
   const goal = 100;
   const points = 71;
+  const hpList = ["Medicaid Waitlist"];
+  const elseList = [
+    "Register for Autism Symposium",
+    "Intensive IEP support & training",
+  ];
+
+  const hpElements = hpList.map((thing, index) => (
+    <p className={styles.list}>{index + 1 + ". " + thing}</p>
+  ));
+  const elseElements = elseList.map((thing, index) => (
+    <p className={styles.list}>{index + 1 + ". " + thing}</p>
+  ));
 
   return (
     <div>
-      <div className = {cx(styles.text_div, "first")}>
-        <text className = {cx(styles.welcome)}>Welcome&nbsp;</text>
-        <text className = {cx(styles.welcome, "family")}>{" " + lastName} Family!</text>
+      <div className={cx(styles.text_div, "first")}>
+        <text className={cx(styles.welcome)}>Welcome&nbsp;</text>
+        <text className={cx(styles.welcome, "family")}>
+          {" " + lastName} Family!
+        </text>
       </div>
-      <div className = {cx(styles.text_div, "second")}>
-        <text className = {cx(styles.cruising)}>You're&nbsp;</text>
-        <text className = {cx(styles.cruising, "color")}>Cruising it!</text>
+      <div className={cx(styles.text_div, "second")}>
+        <text className={cx(styles.cruising)}>You're&nbsp;</text>
+        <text className={cx(styles.cruising, "color")}>Cruising it!</text>
       </div>
-      <div className = {cx(styles.text_div)}>
-        <text className = {cx(styles.points)}>{goal-points}</text>
-        <text className = {cx(styles.points, "text")}>&nbsp;points away from your weekly goal</text>
+      <div className={cx(styles.text_div)}>
+        <text className={cx(styles.points)}>{goal - points}</text>
+        <text className={cx(styles.points, "text")}>
+          &nbsp;points away from your weekly goal
+        </text>
+      </div>
+      <div className={cx(styles.progress_circle)}>
+        <CircularProgressbarWithChildren value={(100 * points) / goal} strokeWidth={16} styles={buildStyles({
+          pathColor: "#E3D150",
+          trailColor: "#F9F6DC",
+        })}>
+          <div className={cx(styles.progress_circle_text)}>{points}</div>
+          <div className={cx(styles.progress_circle_text)}>Points</div>
+        </CircularProgressbarWithChildren>
+      </div>
+      <div className={cx(styles.todo_div)}>
+        <h1 className={cx(styles.radar)}>On Your Radar</h1>
+        <h2 className={cx(styles.priority)}>High Priority</h2>
+        {hpElements}
+        <h2 className={cx(styles.priority, "else")}>Everything Else</h2>
+        {elseElements}
+        <div className={cx(styles.link_div)}>
+          <a href={ROUTES.ROADMAP} className={cx(styles.link)}>
+            See in Roadmap
+          </a>
+        </div>
       </div>
       <NavBar />
     </div>
@@ -61,3 +103,9 @@ export const Home = () => {
 };
 
 export default Home;
+
+//issues:
+//what happens when todo list becomes larger than the screen
+//or family last name is too long
+//fix navbar for computer screen (looks bad on screen anyways)
+//spacing can be tweaked
